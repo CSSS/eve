ba068fcd64ad9cef3768b605743b37501917645d
Ike Chan <tszmingjack@gmail.com>
2023-01-13 18:44:47 -0800

    🔨 console.log syncronises with winstonLogger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,57 @@
 import { SapphireClient as Client } from "@sapphire/framework";
-import logger from "./logger";
+import { InspectOptions } from "util";
+import winstonLogger from "./logger";
+
+// var _log = console.log,
+//     _warn = console.warn,
+//     _error = console.error;
+
+// console.log = function(args: any) {
+//     logger.log('info', args)
+//     _log(console, args);
+// };
+
+// console.warn = function(args: any) {
+//     logger.log('warn', args)
+//     _warn(console, args);
+// };
+
+// console.error = function() {
+//     logger.log('error', arguments);
+//     _error(console, arguments);
+// };
+// define a new console
+const log = console.log;
+const newConsole = {
+    log: function(){
+        const args = Array.from(arguments);
+        // console.info(args);
+        winstonLogger.log('info', args.join(' '));
+        log.apply(console, args);
+    },
+    // info: function (text: string) {
+    //     oldCons.info(text);
+    //     // Your code
+    // },
+    // warn: function (text: string) {
+    //     oldCons.warn(text);
+    //     // Your code
+    // },
+    // error: function (text: string) {
+    //     oldCons.error(text);
+    //     // Your code
+    // }
+};
+
+// Then redefine the old console
+Object.assign(console, newConsole);
 
 const bot = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
 bot.on('ready', () => {
     console.log("Hello")
-    logger.log('info', "Bot is ready.")
     setTimeout(() => {
-        logger.log('info', "One second had passed")
+        console.log('info', "One second had passed")
     }, 1 * 1000);
 });
 
