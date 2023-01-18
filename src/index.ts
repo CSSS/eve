b65831bd99a20e69ca82904399253ade81c8ae93
Ike Chan <tszmingjack@gmail.com>
2023-01-17 20:42:27 -0800

    🔨 Removed commented scripts. console.log now requires level specificiation.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
 import { SapphireClient as Client } from "@sapphire/framework";
-import { InspectOptions } from "util";
 import winstonLogger from "./logger";
 
-// var _log = console.log,
-//     _warn = console.warn,
-//     _error = console.error;
-
-// console.log = function(args: any) {
-//     logger.log('info', args)
-//     _log(console, args);
-// };
-
-// console.warn = function(args: any) {
-//     logger.log('warn', args)
-//     _warn(console, args);
-// };
-
-// console.error = function() {
-//     logger.log('error', arguments);
-//     _error(console, arguments);
-// };
-// define a new console
 const log = console.log;
 const newConsole = {
     log: function(){
         const args = Array.from(arguments);
-        // console.info(args);
-        winstonLogger.log('info', args.join(' '));
-        log.apply(console, args);
+        const level = args.shift();
+        if (args.length > 0) {
+            winstonLogger.log(level, args.join(' '));
+            log.apply(console, args);
+        }
     },
     // info: function (text: string) {
     //     oldCons.info(text);
@@ -42,16 +24,14 @@ const newConsole = {
     //     // Your code
     // }
 };
-
-// Then redefine the old console
 Object.assign(console, newConsole);
 
 const bot = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
 
 bot.on('ready', () => {
-    console.log("Hello")
+    console.log('info', "Hello")
     setTimeout(() => {
-        console.log('info', "One second had passed")
+        console.log('info2', "One second had passed")
     }, 1 * 1000);
 });
 
