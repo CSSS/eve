9fb6bcfbbf534d2f8e17ee8ba7496fda824937d7
Ike Chan <tszmingjack@gmail.com>
2023-01-26 18:22:31 -0800

    💥 Moving 'modify global console' to index.ts temporarily

--- src/botClient.ts
+++ src/botClient.ts
@@ -9,3 +9,4 @@ function start() {
 }
 
 export { start, bot };
+
--- src/logger.ts
+++ src/logger.ts
@@ -24,31 +24,5 @@ const winstonLogger = createLogger({
     ]
 })
 
-// modify the global console
-const log = console.log;
-const newConsole = {
-    log: function(){
-        const args = Array.from(arguments);
-        const customName = args.shift();
-        if (args.length > 0) {
-            console.info(`Logging with custom new name: ${customName}`)
-            
-            winstonLogger.configure({
-                transports: defaultWinstonInfoFileTransports({ filename: `logs/${customName}.log` }),
-            })
-            winstonLogger.log('info', args.join(' '));
-            log.apply(console, args);
-        }
-        else {
-            console.info(`Returning to default transports`)
-            
-            winstonLogger.configure({
-                transports: defaultWinstonInfoFileTransports()
-            })
-            winstonLogger.log('info', customName);
-            log(customName);
-        }
-    },
-};
-Object.assign(console, newConsole);
+export { winstonLogger, defaultWinstonInfoFileTransports };
 
