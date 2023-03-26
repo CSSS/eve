f21526ca14ae4530d70ced23e990d8cd3c97088b
ikec <tszmingjack@gmail.com>
2023-03-26 16:21:51 -0700

    :art: Reorganising the logger file

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,59 +1,73 @@
-import { createLogger, format as WinstonFormat, transports as WinstonTransports } from "winston";
+import { createLogger, format as WinstonFormat, Logger, transports as WinstonTransports } from 'winston';
 import { FileTransportOptions } from "winston/lib/winston/transports";
 import { strftime } from "../strftime";
-import { Watcher } from "./watcher";
-
-export let logsPath = "logs/";
-export let loggerNameSuffixInfo = "-info";
-export let loggerNameSuffixError = "-error";
-export let logFileExtension = ".log";
-
-export let process_time : String | null = null;
 
 // set up winston logger
 namespace WinstonLogger {
+    //#region CONST
+    export const LOG_PATH = "logs/";
+    export const LOG_SFX_INF = "-info";
+    export const LOG_SFX_ERR = "-error";
+    export const LOG_EXT = ".log";
     const {combine, timestamp, printf} = WinstonFormat
-    const myTimestamp = () => timestamp({format: 'YYYY-MM-DD HH:mm:ss'})
-    const myFormat = printf((parms) => {
+    const DEFAULT_TS = () => timestamp({format: 'YYYY-MM-DD HH:mm:ss'})
+    const DEFAULT_FORM = printf((parms) => {
         return `[${parms.timestamp}] ${parms.level}: ${parms.message}`;
     });
-    const defaultWinstonInfoFileTransports = (cmd: any = null, options: Partial<FileTransportOptions> = {}) => {
+    //#endregion
+
+    export let process_time : String | null = null;
+    export let eve_logger: Logger | null = null;
+    const DEFAULT_INFO_TSF = (cmd: string = 'unknown', options: Partial<FileTransportOptions> = {}) => {
         return new WinstonTransports.File(Object.assign({
-            filename: `${logsPath}${process_time}-${cmd}${loggerNameSuffixInfo}${logFileExtension}`,
+            filename: `${LOG_PATH}${process_time}-${cmd}${LOG_SFX_INF}${LOG_EXT}`,
             level: 'info',
-            format: combine(myTimestamp(), myFormat)
+            format: combine(DEFAULT_TS(), DEFAULT_FORM)
         }, options))
     }
-    const defaultWinstonErrorsFileTransports = (cmd : any= null, options: Partial<FileTransportOptions> = {}) => new WinstonTransports.File(Object.assign({
-        filename: `${logsPath}${process_time}-${cmd}${loggerNameSuffixError}${logFileExtension}`,
+    const DEFAULT_ERR_TSF = (cmd : string = 'unknown', options: Partial<FileTransportOptions> = {}) => new WinstonTransports.File(Object.assign({
+        filename: `${LOG_PATH}${process_time}-${cmd}${LOG_SFX_ERR}${LOG_EXT}`,
         level: 'error',
-        format: combine(myTimestamp(), myFormat)
+        format: combine(DEFAULT_TS(), DEFAULT_FORM)
     }, options))
+
     export const Initialise = async (cmd: string = 'eve') => {
+        console.info(`Initialising logger for ${cmd}...`)
+        // if process time is null, this is the first initialise call.
+        // modify global console in the first call. once is enough.
+        // [modifying global console to redirect any console calls to eve_info / eve_error logger]
         if (process_time === null) {
+            // set process time first to confirm the first stack of Initialise has already been called.
             process_time = strftime("%G-%m-%d_%I-%M-%S-%p", new Date())
+
+            // initialise eve logger
+            eve_logger = await Initialise();
+            
+            // make console.log call winston logger as well.
+            const clog = console.log;
+            Object.assign(console, {
+                log: function() {
+                    const args = Array.from(arguments).map(
+                        a => a?.toString()
+                    ).join(', ');
+                    clog(args);
+                    eve_logger?.info(args)
+                }
+            })
+
         }
 
         const winstonLogger = createLogger({
             transports: [
-                defaultWinstonInfoFileTransports(cmd),
-                defaultWinstonErrorsFileTransports(cmd),
+                DEFAULT_INFO_TSF(cmd),
+                DEFAULT_ERR_TSF(cmd),
             ]
-        })
+        });
 
-        if (cmd === "eve") {
-            // modify the global console
-            const originLog = console.log;
-            Object.assign(console, {
-                log: function (args: any) {
-                    winstonLogger.info(args);
-                    originLog(args);
-                }
-            });
-            await Watcher.Add(cmd)
-        } else {
-            await Watcher.Add(cmd)
-        }
+        const winstonInfo = winstonLogger.info;
+        const winstonErr = winstonLogger.error;
+
+        console.info("Done.");
         return winstonLogger;
     }
 }
